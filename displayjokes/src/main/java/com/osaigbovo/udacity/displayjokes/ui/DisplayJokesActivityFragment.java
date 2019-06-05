6678e27a9176c35c9c1f892e4b582ef167a90530
Osaigbovo <odiase.osaigbovo@gmail.com>
2019-06-06 00:36:15 +0100

    free productFlavour

--- displayjokes/src/main/java/com/osaigbovo/udacity/displayjokes/ui/DisplayJokesActivityFragment.java
+++ displayjokes/src/main/java/com/osaigbovo/udacity/displayjokes/ui/DisplayJokesActivityFragment.java
@@ -1,22 +1,44 @@
 package com.osaigbovo.udacity.displayjokes.ui;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.osaigbovo.udacity.displayjokes.R;
 
 public class DisplayJokesActivityFragment extends Fragment {
 
-    public DisplayJokesActivityFragment() {}
+    public static final String FUNNY_JOKE = "funny_joke";
+
+    private String joke;
+
+    public DisplayJokesActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.joke = getArguments().getString(FUNNY_JOKE);
+        }
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_display_jokes, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_display_jokes, container, false);
+
+        TextView jokeDetails = rootView.findViewById(R.id.text_display_jokes);
+        jokeDetails.setText(joke);
+
+        return rootView;
     }
+
+
 }
